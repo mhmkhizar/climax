68f7d7edd8e2a3b44d29c840c4fd28a219fe801b
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-09-28 20:59:11 +0500

    Initial commit

---
+++ webpack.dev.cjs
@@ -0,0 +1,12 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.cjs");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  // devtool: "source-map",
+  // devtool: "cheap-module-source-map",
+  devServer: {
+    watchFiles: ["./src/main.html"],
+  },
+});
